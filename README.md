095b337f3acb6e1e4aa56dd421bef29d9d53335d
DivasRegmi <divasregmi@gmail.com>
2020-07-03 22:57:22 +0545

    Works for route

--- README.md
+++ README.md
@@ -5,7 +5,5 @@ This is simple ***E-COMMERSE*** app just for *learning* purpose.
 ## Features-used
 
 - React
-- React Routing
-- Redux
-- firebase
-- APIs for PayPal
+- React Router
+
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,23 +1,28 @@
 import React from "react";
 import cx from "classnames";
 
+import { withRouter } from "react-router-dom";
+
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={cx("menu-item", size)}>
-
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) =>{ 
+  return(
+  <div
+    className={cx("menu-item", size)}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     <div
       className="backgroundImage"
       style={{
         backgroundImage: `url(${imageUrl})`,
       }}
     />
-  
+
     <div className="content">
       <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
-);
+)};
 
-export default MenuItem;
+export default withRouter(MenuItem);
