0c0747330d384419f6016eb85c168e3fe7f5a564
DivasRegmi <divasregmi@gmail.com>
2020-07-12 12:27:12 +0545

    added cart-icon and cart-dropdown component to our app and also implement cart reducer

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {connect} from 'react-redux'
+import { connect } from "react-redux";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
-import {auth} from '../../firebase/firebase.utils'
+import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CarrDropdown from "../cart-dropdown/cart-dropdown.component";
 
 import "./header.styles.scss";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-function Header({ currentUser }) {
+function Header({ currentUser, hidden }) {
   return (
     <div className="header">
       <Link to="/" className="logo-container">
@@ -23,19 +26,26 @@ function Header({ currentUser }) {
         </Link>
 
         {currentUser ? (
-          <div className='option' onClick={()=> auth.signOut()}>SIGN OUT</div>
+          <div className="option" onClick={() => auth.signOut()}>
+            SIGN OUT
+          </div>
         ) : (
           <Link className="option" to="/signIn">
             SIGN IN
           </Link>
         )}
+
+        <CartIcon />
       </div>
+
+      {hidden ? null : <CartDropdown />}
     </div>
   );
 }
 
-const mapStateTOProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateTOProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
-export default connect(mapStateTOProps)(Header)
+export default connect(mapStateTOProps)(Header);
