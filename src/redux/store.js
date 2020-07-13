6168889f4dd1f608f19c921d7d86dcbc4121c511
D BOSS <divasregmi@gmail.com>
2020-07-13 17:21:28 +0545

    implement redux-persist set-up

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,6 @@
 import { createStore, applyMiddleware, compose } from "redux";
+import {persistStore} from 'redux-persist'
+
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
@@ -7,13 +9,15 @@ import rootReducer from "./root-reducer";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const middlewares = [logger];
 
-const store = createStore(rootReducer, 
+ export const store = createStore(rootReducer, 
     composeEnhancers(
     applyMiddleware(...middlewares)
   ));
 
 
-export default store;
+ export const persistor = persistStore(store);
+
+// export default { store, persistor };
 
  
 
