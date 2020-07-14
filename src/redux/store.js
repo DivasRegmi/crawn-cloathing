23f089e05de0cdf938472ceb8e633d20edeb7412
D BOSS <divasregmi@gmail.com>
2020-07-14 19:42:29 +0545

    remove redux-loger and redux dev-tool from production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,23 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import {persistStore} from 'redux-persist'
-
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
+let composeEnhancers = compose;
+const middlewares = [];
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const middlewares = [logger];
+if(process.env.NODE_ENV === 'development'){
+  middlewares.push(logger)
+  composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
- export const store = createStore(rootReducer, 
-    composeEnhancers(
-    applyMiddleware(...middlewares)
-  ));
+}
 
+export const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(...middlewares))
+);
 
- export const persistor = persistStore(store);
+export const persistor = persistStore(store);
 
-// export default { store, persistor };
-
- 
 
